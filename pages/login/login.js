a950f9beba35da3405c9fc0ae0b8648d56ddf26b
Jane <582194643@qq.com>
2020-04-01 16:43:38 +0800

    手机号登录页面-布局

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,7 +2,6 @@
 Page({
   // 微信授权登录
   getUserInfo(e){
-    console.log(e);
     // 当授权被拒绝时，返回
     if (e.detail.errMsg==='getUserInfo:fail auth deny') {
       return;
@@ -24,7 +23,6 @@ Page({
           },
           // 接口调用成功的回调函数
           success (res) {
-            console.log(res.data)
             // 弹出消息
             wx.showToast({
               title: res.data.message,
@@ -34,7 +32,7 @@ Page({
             if (res.data.status===0) {
               // 保存token到本地
               wx.setStorageSync('token', res.data.token);
-              // 跳转到首页
+              // 跳转到首页（关闭所有页面，打开到应用内的某个页面）
               wx.reLaunch({
                 url: '/pages/home/home',
               });
@@ -48,66 +46,11 @@ Page({
       }
     })
   },
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 手机号登录
+  goToPhoneLogin(){
+    // 跳转到手机号登录页面
+    wx.navigateTo({
+      url: '/pages/phone-login/phone-login',
+    })
   }
 })
