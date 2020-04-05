8638d5ed3e215a3737e8fadd88486eb162521a7f
Jane <582194643@qq.com>
2020-04-05 15:51:40 +0800

    解决bug-登录后刷新跳转到首页显示异常

--- app.js
+++ app.js
@@ -2,12 +2,5 @@
 App({
   // 生命周期回调——监听小程序初始化
   onLaunch: function () {
-    // 如果本地存储有token
-    if (wx.getStorageSync('token')) {
-      // 跳转到首页
-      wx.reLaunch({
-        url: '/pages/home/home',
-      })
-    }
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,35 +1,44 @@
 // pages/login/login.js
 Page({
+  onLoad: function () {
+    // 如果本地存储有token
+    if (wx.getStorageSync('token')) {
+      // 跳转到首页
+      wx.reLaunch({
+        url: '/pages/home/home',
+      })
+    }
+  },
   // 微信授权登录
-  getUserInfo(e){
+  getUserInfo(e) {
     // 当授权被拒绝时，返回
-    if (e.detail.errMsg==='getUserInfo:fail auth deny') {
+    if (e.detail.errMsg === 'getUserInfo:fail auth deny') {
       return;
     }
     // 授权成功，获取用户头像和用户名(es6解构赋值，重命名)
-    const {avatarUrl:avatar,nickName:nickname} = e.detail.userInfo;
+    const { avatarUrl: avatar, nickName: nickname } = e.detail.userInfo;
     // 获取登录的临时code
     wx.login({
       // 接口调用成功的回调函数
-      success: ({code}) => {
+      success: ({ code }) => {
         // 发送网络请求
         wx.request({
-          url: 'http://localhost:3000/api/user/wxlogin', 
-          method:'POST',
+          url: 'http://localhost:3000/api/user/wxlogin',
+          method: 'POST',
           data: {
             code,
             nickname,
             avatar
           },
           // 接口调用成功的回调函数
-          success (res) {
+          success(res) {
             // 弹出消息
             wx.showToast({
               title: res.data.message,
-              icon:'none'
+              icon: 'none'
             });
             // 当返回状态为登录成功时
-            if (res.data.status===0) {
+            if (res.data.status === 0) {
               // 保存token到本地
               wx.setStorageSync('token', res.data.token);
               // 跳转到首页（关闭所有页面，打开到应用内的某个页面）
@@ -41,13 +50,13 @@ Page({
         })
       },
       // 接口调用失败的回调函数
-      fail:err=>{
+      fail: err => {
         console.log('获取code失败');
       }
     })
   },
   // 手机号登录
-  goToPhoneLogin(){
+  goToPhoneLogin() {
     // 跳转到手机号登录页面
     wx.navigateTo({
       url: '/pages/phone-login/phone-login',
