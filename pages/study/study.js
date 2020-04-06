a61bbdef8d57761b242e9e6477ffd56394b688f4
Jane <582194643@qq.com>
2020-04-06 15:28:52 +0800

    学习页面-布局+学习进度列表数据获取+使用vant-weapp中的circle

--- pages/study/study.js
+++ pages/study/study.js
@@ -1,66 +1,43 @@
 // pages/study/study.js
+import request from '../../utils/request';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 学习进度数据
+    studyProgress: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取学习进度列表
+    this.getProgressData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async getProgressData() {
+    // 发请求，获取学习进度列表
+    const res = await request({
+      url: 'study/progress'
+    });
+    // 当请求成功后
+    if (res.data.status === 0) {
+      // 遍历数组，动态添加color属性
+      res.data.message.forEach(item => {
+        if (item.study_progress <= 30) {
+          item.color = "#ff0000";
+        } else if (item.study_progress > 30 && item.study_progress <= 60) {
+          item.color = "#FF783B";
+        }else{
+          item.color = "#B4D66E";
+        }
+      });
+      // 保存学习进度数据
+      this.setData({
+        studyProgress: res.data.message
+      })
+    }
   }
 })
